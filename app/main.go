2f5e9cefc43393f974e7f0dacedf9b6f9c1557fa
piojagcodes <piojag@gmail.com>
2023-05-24 11:36:14 +0200

    writing comments for whole code

--- app/main.go
+++ app/main.go
@@ -21,6 +21,10 @@ func newRouter() *httprouter.Router {
 
 }
 
+
+// getChannelStats defines the handler function for the "/youtube/channel/stats" endpoint
+// It writes a response with the text "response!" to the HTTP response writer. 
+
 func getChannelStats() httprouter.Handle {
 
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -28,24 +32,32 @@ func getChannelStats() httprouter.Handle {
 	}
 }
 
+
 func main() {
+	// Create a new HTTP server.
 	srv := &http.Server{
-		Addr: ":10101",
-		Handler: newRouter(),
+		Addr: ":10101",		// Listen on port 10101.
+		Handler: newRouter(),	// Use the router returned by newRouter()
 	}
 
 	idleConnsClosed := make(chan struct{})
+
+	// Start a goroutine to handle server shutdown
 	go func() {
+		// Listen for OS interrupt signals (e.g. Ctrl+C)
 		sigint := make(chan os.Signal, 1)
 		signal.Notify(sigint, os.Interrupt)
 		signal.Notify(sigint, syscall.SIGTERM)
 		<-sigint
 
-		log.Println("service interrupt received")
+		log.Println("service interrupt received")i
 
+		// Create a context with a timeout of 60 seconds for server shutdown.
 		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 		defer cancel()
 
+
+		// Shut down the server gracefully	
 		if err := srv.Shutdown(ctx); err != nil {
 			log.Printf("http server shutdown error: %v", err)
 		}
@@ -57,7 +69,10 @@ func main() {
 	}()
 
 	log.Printf("Starting server on port 10101")
+
+	// Start the HTTP server and listen for incoming requests.
 	if err := srv.ListenAndServe(); err != nil {
+		// Check if server was closed intentionally.
 		if !errors.Is(err, http.ErrServerClosed){
 			log.Fatalf("fatal http server failed to start: %v", err)
 		
